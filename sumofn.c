c4e72c3be31ab745e0fb8ab8fe1e4000514a6837
DAravindReddy <aravindreddy0726@gmail.com>
2026-06-03 11:28:05 +0530

    modified file

--- sumofn.c
+++ sumofn.c
@@ -1,8 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-	int n = 100;
-	int sum = (n(n+1))/2;
-	printf("Sum of n numbers: %d\n", sum);
-	return 0;
+    int n = 100;
+    int sum = (n * (n + 1)) / 2;
+
+    printf("Sum of first %d numbers: %d\n", n, sum);
+
+    return 0;
 }

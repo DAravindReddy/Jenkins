ca6fcda806db7a3cfc64b10867d66cbd29a073f9
DAravindReddy <aravindreddy0726@gmail.com>
2026-06-03 17:48:41 +0530

    cd jenkins

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
+/*
+*#include<stdio.h>
+*int main()
+*{
+*	int n1 = 10, n2 = 20;
+*	int res = 0;
+*	printf("sum of n1 and n2 = %d\n", res = n1 + n2);
+*}
+*/
+#include <stdio.h>
 
-#include<stdio.h>
-int main()
-{
-	int n1 = 10, n2 = 20;
-	int res = 0;
-	printf("sum of n1 and n2 = %d\n", res = n1 + n2);
+int main() {
+    printf("Hello Jenkins CD!\n");
+    return 0;
 }
